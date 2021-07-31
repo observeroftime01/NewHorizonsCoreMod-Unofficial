9996365057c140f6b073869a09ea0bfc485ab0b5
observeroftime02 <observeroftime@shizuki.ca>
2021-07-30 18:15:08 -0700

    Changed autoclave recipe for raw crystal chips to always yield output when used with flawless gems (was 10% chance in low grav before), changed login MOTD to make it clear this is not an official release and instead my own version.

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -11,19 +11,11 @@ public class LoginHandler {
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
     	event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=====================================================" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to Gregtech: New Horizons " + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE  + "The Quest Book has a shortcut key, check your keybindings" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE  + "to remove conflicts." ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "New features include: Using cells on machine GUI tanks for"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN	+ "I/O, NEI having tabs at the top and multiple recipes on a "));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "page, Cooking for Blockheads addition, Logistics Pipes"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "addition, Tungsten change, glowy textures (your resourcepack"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN	+ "will need to be updated to do this), other texture work you"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "probably won't notice but still looks nice, quest changes, and a"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "whole bunch of other stuff, so check out the changelog."));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Please report bugs here:" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Visit our discord at https://discord.gg/EXshrPV" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to Gregtech: New Horizons " + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion + "-ex"));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "DO NOT REPORT BUGS TO OFFICIAL GTNH MODPACK DEVS" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "THIS IS A CUSTOM BUILD NOT MEANT FOR GENERAL USE BY THE PUBLIC" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Instead, drop an issue on the GitHub Page if you care"));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "https://github.com/observeroftime01/NewHorizonsCoreMod-Unofficial" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=====================================================" ));
     }
 }
